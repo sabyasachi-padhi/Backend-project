8b004c13f1656f49763815f156ab54ff7b94b2c5
sabyasachi padhi <padhisabyasachi354@gmail.com>
2025-07-05 21:17:00 +0530

    Fix: Corrected .env path in dotenv config, MongoDB connected successfully

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-
-
-
+dotenv.config({path:'./.env'})
+connectDB()
 
 
 
