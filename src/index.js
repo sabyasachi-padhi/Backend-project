29671dd99cf605cbb819330bc4e9716d2aad44f4
sabyasachi padhi <padhisabyasachi354@gmail.com>
2025-07-07 11:12:23 +0530

    Refactor: Moved .gitkeep to public/temp and removed from other empty directories

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@
 import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 dotenv.config({path:'./.env'})
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`app listen on port :${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB CONNECTION FAILED",error);
+    
+})
 
 
 
